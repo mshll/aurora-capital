383ab17d82d85ea7b08807126b8876427406eb60
jannakam <jannakalmuqaisib@gmail.com>
2024-10-28 15:16:22 +0300

    deposit/withdraw

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className='flex h-full flex-1 flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20'>
       {/* <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>{metadata.title}</h1>
       <p className='leading-7'>Great things are coming soon!</p> */}
-      <SelfPay />
+      {/* <SelfPay /> */}
     </div>
   );
 }
