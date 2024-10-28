2a20ce104e540fe375274f6599243f4d8a62782c
meshal <imeshalfm@gmail.com>
2024-10-28 12:58:33 +0300

    change users list UI

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,9 +1,10 @@
 import { getAllUsers } from '@/actions/users';
 import GetAllUsers from '@/components/GetAllUser';
+import { baseUrl } from '@/actions/config';
 
 async function Users() {
   const allUsers = await getAllUsers();
-  return <GetAllUsers users={allUsers} />;
+  return <GetAllUsers baseUrl={baseUrl} users={allUsers} />;
 }
 
 export default Users;
--- src/components/GetAllUser.jsx
+++ src/components/GetAllUser.jsx
@@ -13,15 +13,14 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 import { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Search, UserIcon } from 'lucide-react';
-import Transactions from './Transactions';
+import { CornerUpRightIcon, Search, UserIcon } from 'lucide-react';
 import TransferForm from './TransferForm';
 
-function GetAllUsers({ users }) {
+function GetAllUsers({ baseUrl, users }) {
   const [query, setQuery] = useState('');
   const [filter, setFilter] = useState('all');
 
@@ -32,51 +31,59 @@ function GetAllUsers({ users }) {
   });
 
   const userList = filteredUsers.map((user) => {
+    const balance = Number.isInteger(user.balance) ? user.balance : user.balance.toFixed(3);
     return (
       <Card key={user._id}>
-        <CardHeader className='flex-col items-center justify-between'>
-          <CardTitle className='text-2xl font-medium'>{user.username}</CardTitle>
-          <Avatar className='h-14 w-14'>
-            <AvatarImage src={`https://react-bank-project.eapi.joincoded.com/${user.image}`} alt='ACB' />
-            <AvatarFallback>
-              <UserIcon className='h-5 w-5' />
-            </AvatarFallback>
-          </Avatar>
-        </CardHeader>
-
-        <CardContent>
-          <div className='flex flex-col items-center space-x-4'>
-            <div className='flex flex-col space-y-1'>
-              <p className='text-sm text-muted-foreground'>Balance: ${user.balance.toFixed(2)}</p>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant='outline'>Transfer</Button>
-                </DialogTrigger>
-                <DialogContent className='sm:max-w-[425px]'>
-                  <DialogHeader>
-                    <DialogTitle>Transfer</DialogTitle>
-                    <DialogDescription>Enter the amount you'd like to transfer to {user.username} </DialogDescription>
-                  </DialogHeader>
-                  <div className='grid gap-4 py-4'>
-                    <div className='flex'>
-                      {/* <Label htmlFor='name' className='text-right'>
-                        Amount
-                      </Label> */}
-                      {/*  */}
-                      <TransferForm username={user.username} />
-
-                      {/*  */}
-                    </div>
-                  </div>
-                  <DialogFooter>
-                    <DialogClose asChild>
-                      <Button>Cancel</Button>
-                    </DialogClose>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
+        <CardContent className='flex items-center justify-start gap-4 px-6 py-6'>
+          <div className='flex flex-1 flex-row items-center justify-start gap-4 overflow-hidden text-ellipsis'>
+            <Avatar className='h-12 w-12'>
+              <AvatarImage src={`${baseUrl}/${user.image}`} alt='User' className='bg-muted' />
+              <AvatarFallback>
+                <UserIcon className='h-5 w-5' />
+              </AvatarFallback>
+            </Avatar>
+            <div className='flex flex-col gap-1 overflow-hidden text-ellipsis'>
+              <h3 className='overflow-hidden text-ellipsis text-xl font-medium'>{user.username}</h3>
+              <p
+                className={`text-sm ${balance > 1000000 ? 'font-semibold text-amber-700 dark:text-amber-500' : 'text-muted-foreground'}`}
+              >
+                {balance > 0 ? '$' + balance : 'Broke :('}
+              </p>
             </div>
           </div>
+
+          <Dialog>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <DialogTrigger asChild>
+                    <div className='hover:cursor-pointer'>
+                      <CornerUpRightIcon className='h-5 w-5' />
+                    </div>
+                  </DialogTrigger>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Transfer funds to {user.username}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <DialogContent className='sm:max-w-[425px]'>
+              <DialogHeader>
+                <DialogTitle>Transfer</DialogTitle>
+                <DialogDescription>Enter the amount you&apos;d like to transfer to {user.username}</DialogDescription>
+              </DialogHeader>
+              <div className='grid gap-4 py-4'>
+                <div className='flex'>
+                  <TransferForm username={user.username} />
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button>Cancel</Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </CardContent>
       </Card>
     );
@@ -85,12 +92,10 @@ function GetAllUsers({ users }) {
   return (
     <div className='flex h-full flex-1 flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20'>
       <div className='no-scrollbar h-[500px] w-[900px] overflow-y-auto rounded-lg border bg-card p-6 text-card-foreground shadow-sm'>
-        <div className='mb-6 flex flex-col space-y-1.5'>
-          <h3 className='text-2xl font-semibold leading-none tracking-tight'>User List</h3>
-        </div>
-        <div className='flex flex-col space-y-4'>
-          <div className='flex flex-col space-y-2 sm:flex-row sm:items-center sm:justify-between sm:space-x-2 sm:space-y-0'>
-            <div className='relative w-full sm:max-w-sm'>
+        <div className='mb-6 flex w-full items-center justify-center gap-2'>
+          <h3 className='flex-1 text-2xl font-semibold leading-none tracking-tight'>User List</h3>
+          <div className='flex flex-1 flex-col space-y-2 sm:flex-row sm:items-center sm:justify-between sm:space-x-2 sm:space-y-0'>
+            <div className='relative w-full'>
               <Search className='absolute left-2 top-2.5 h-4 w-4 text-muted-foreground' />
               <Input
                 placeholder='Search for a user'
@@ -111,8 +116,9 @@ function GetAllUsers({ users }) {
               </SelectContent>
             </Select> */}
           </div>
-
-          <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>{userList}</div>
+        </div>
+        <div className='flex flex-col space-y-4'>
+          <div className='grid gap-4 md:grid-cols-1 lg:grid-cols-2'>{userList}</div>
         </div>
       </div>
     </div>
