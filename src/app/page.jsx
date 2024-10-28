d91a7c5f9921d49dd4fc856933c1aa83f7e64215
jannakam <jannakalmuqaisib@gmail.com>
2024-10-28 15:13:52 +0300

    deposit/withdraw

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
+import SelfPay from '@/components/SelfPay';
 import { metadata } from './layout';
 
 export default function Home() {
   return (
     <div className='flex h-full flex-1 flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20'>
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>{metadata.title}</h1>
-      <p className='leading-7'>Great things are coming soon!</p>
+      {/* <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>{metadata.title}</h1>
+      <p className='leading-7'>Great things are coming soon!</p> */}
+      <SelfPay />
     </div>
   );
 }
--- src/components/SelfPay.jsx
+++ src/components/SelfPay.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import { ChevronsUpDown } from "lucide-react"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import DepositToAccount from "./Deposit"
+import WithdrawToAccount from "./Withdraw"
 
-function SelfPay() {
-  return (
-    <div>
+async function SelfPay() {
+
+    return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Deposit</TabsTrigger>
+        <TabsTrigger value="password">Withdraw</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Deposit</CardTitle>
+            <CardDescription>
+              Deposit funds into your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <DepositToAccount />
+          </CardContent>
+        </Card>
+      </TabsContent>
+
       
-    </div>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Withdraw</CardTitle>
+            <CardDescription>
+            Withdraw funds from your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <WithdrawToAccount />
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
   )
 }
 
