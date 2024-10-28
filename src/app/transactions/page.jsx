b8345f009d169f387417457af78e189b165d5ba9
jannakam <jannakalmuqaisib@gmail.com>
2024-10-28 13:27:13 +0300

    transaction table

--- src/app/transactions/page.jsx
+++ src/app/transactions/page.jsx
@@ -1,12 +1,15 @@
 import { getUser } from '@/actions/token';
 import { myTransactions } from '@/actions/transactions';
 import Transactions from '@/components/Transactions';
+import TransactionTable from '@/components/TransactionTable';
 
 async function TransactionsPage() {
   const transactions = await myTransactions();
   const user = await getUser();
 
-  return <Transactions transactions={transactions} user={user} />;
+  return <TransactionTable transactions={transactions} user={user} />;
+  // return <Transactions transactions={transactions} user={user} />;
+
 }
 
 export default TransactionsPage;
